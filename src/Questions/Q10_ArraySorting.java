2d07b49dc06e68c32ea8e8cf18d56051d87c037e
Souvik <souviksantra1999@gmail.com>
2024-12-16 22:19:16 +0530

    Insertion sort code added

--- src/Questions/Q10_ArraySorting.java
+++ src/Questions/Q10_ArraySorting.java
@@ -2,15 +2,46 @@ package Questions;
 
 public class Q10_ArraySorting {
 	public static void main(String[] args) {
+		
+		// Writing a program to implement sorting elements in an unsorted array
 		int[] numbers = {3,2,4,1};
-		bubbleSort(numbers);
+		//bubbleSort(numbers);
 		
 		int low = 0;
 		int high = numbers.length - 1;
-		quickSort(numbers,low,high);
+		//quickSort(numbers,low,high);
+		//selectionSort(numbers);
+		insertionSort(numbers);
 		System.out.print("After sorting elements using Quick sort  : ");
 		for(int n :numbers) {
 			System.out.print(n+" ");
+		}
+	}
+
+	private static void insertionSort(int[] numbers) {
+		for(int i = 1; i<numbers.length; i++) {
+			int temp = numbers[i];
+			int j = i;
+			while(j>0 && numbers[j-1]>temp) {
+				numbers[j] = numbers[j-1];
+				j = j - 1;
+			}
+			numbers[j] = temp;
+			
+		}
+	}
+
+	private static void selectionSort(int[] numbers) {
+		for(int i = 0; i<numbers.length; i++) {
+			int minIndex = i;
+			for(int j = i+1; j<numbers.length; j++) {
+				if(numbers[j]<numbers[minIndex]) {
+					minIndex=j;
+				}
+			}
+			int temp = numbers[minIndex];
+			numbers[minIndex] = numbers[i];
+			numbers[i] = temp;
 		}
 	}
 
@@ -50,11 +81,11 @@ public class Q10_ArraySorting {
 	private static void bubbleSort(int[] numbers) {
 		int length = numbers.length;
 		for(int i=0; i<length; i++) {
-			for(int j = i+1; j<length ; j++) {
-				if(numbers[i]>numbers[j]) {
-					int temp = numbers[i];
-					numbers[i] = numbers[j];
-					numbers[j] = temp;
+			for(int j = 0; j<length-i-1 ; j++) {
+				if(numbers[j]>numbers[j+1]) {
+					int temp = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = temp;
 				}
 				System.out.print("i="+i+" j="+j +": " );
 				for(int n :numbers) {
